cc41e15acddb321290e861fc282275145d4988e8
Nilah Ravi Nair <nilahnair.27@gmail.com>
2024-01-02 12:14:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ import datetime
 from sacred import Experiment
 from sacred.observers import MongoObserver
 
-ex= Experiment('lara cnn-imu trial')
+ex= Experiment('lara_imu cnn-imu trial')
 
 ex.observers.append(MongoObserver.create(url='curtiz',
                                          db_name='nnair_sacred',
@@ -320,7 +320,7 @@ def setup_experiment_logger(logging_level=logging.DEBUG, filename=None):
 @ex.config
 def my_config():
     print("configuration function began")
-    config = configuration(dataset_idx=0,
+    config = configuration(dataset_idx=1,
                            network_idx=2,
                            output_idx=0,
                            usage_modus_idx=0,
