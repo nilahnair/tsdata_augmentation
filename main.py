e01c994f99da62fd1484aa7f29ecda83423bd24a
Nilah Ravi Nair <nilahnair.27@gmail.com>
2024-01-08 16:50:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ import datetime
 from sacred import Experiment
 from sacred.observers import MongoObserver
 
-ex= Experiment('mobiact cnn_trans 100-0.0001-6 transt')
+ex= Experiment('motionsense cnn_trans 100-0.0001-6 transt')
 
 ex.observers.append(MongoObserver.create(url='curtiz',
                                          db_name='nnair_sacred',
@@ -210,7 +210,7 @@ def configuration(dataset_idx, network_idx, output_idx, usage_modus_idx=0, datas
         folder_exp = {'mocap': "/data/nnair/icpr2024/lara/results/transt/",
                     'mbientlab': "/data/nnair/icpr2024/lara_imu/results/transt/",
                     'mobiact': "/data/nnair/icpr2024/mobiact/results/transt/",
-                    'motionsense': "/data/nnair/icpr2024/motionsense/results/trial/",
+                    'motionsense': "/data/nnair/icpr2024/motionsense/results/transt/",
                     'sisfall': "/data/nnair/icpr2024/sisfall/results/trial1/"
                     }
     elif output[output_idx] == 'attribute':
@@ -229,7 +229,7 @@ def configuration(dataset_idx, network_idx, output_idx, usage_modus_idx=0, datas
                     }
 
     # GPU
-    os.environ["CUDA_VISIBLE_DEVICES"] = "7"
+    os.environ["CUDA_VISIBLE_DEVICES"] = "6"
     GPU = 0
 
     # Labels position on the segmented window
@@ -341,7 +341,7 @@ def setup_experiment_logger(logging_level=logging.DEBUG, filename=None):
 @ex.config
 def my_config():
     print("configuration function began")
-    config = configuration(dataset_idx=2,
+    config = configuration(dataset_idx=3,
                            network_idx=3,
                            output_idx=0,
                            usage_modus_idx=0,
