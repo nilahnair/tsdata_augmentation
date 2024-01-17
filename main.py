82584face36d40285c98086d043adb3190ccbc4f
Nilah Ravi Nair <nilahnair.27@gmail.com>
2024-01-17 16:12:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -229,7 +229,7 @@ def configuration(dataset_idx, network_idx, output_idx, usage_modus_idx=0, datas
                     }
 
     # GPU
-    os.environ["CUDA_VISIBLE_DEVICES"] = "3"
+    os.environ["CUDA_VISIBLE_DEVICES"] = "4"
     GPU = 0
 
     # Labels position on the segmented window
