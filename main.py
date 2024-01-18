1fc0fb09565aadcf12785c1e006b786db304150f
Nilah Ravi Nair <nilahnair.27@gmail.com>
2024-01-18 10:07:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ import datetime
 from sacred import Experiment
 from sacred.observers import MongoObserver
 
-ex= Experiment('lara_imu cnn_trans 100-0.0001-50 transt layer 3')
+ex= Experiment('lara_imu cnn_trans 20-0.0001-50 transt layer 3')
 
 ex.observers.append(MongoObserver.create(url='curtiz',
                                          db_name='nnair_sacred',
@@ -145,12 +145,12 @@ def configuration(dataset_idx, network_idx, output_idx, usage_modus_idx=0, datas
         'cnn': {'mocap': 100, 'mbientlab': 100, 'mobiact': 100, 'motionsense': 50, 'sisfall': 50},
         'lstm': {'mocap': 100, 'mbientlab': 50, 'mobiact': 50, 'motionsense': 100, 'sisfall': 50},
         'cnn_imu': {'mocap': 100, 'mbientlab': 100, 'mobiact': 100, 'motionsense': 100, 'sisfall': 100},
-        'cnn_transformer': {'mocap': 100, 'mbientlab': 100, 'mobiact': 50, 'motionsense': 100, 'sisfall': 50}}
+        'cnn_transformer': {'mocap': 100, 'mbientlab': 20, 'mobiact': 50, 'motionsense': 100, 'sisfall': 50}}
 
     batch_size_val = {'cnn': {'mocap': 100, 'mbientlab': 100, 'mobiact': 100, 'motionsense': 50,'sisfall': 50},
                       'lstm': {'mocap': 100, 'mbientlab': 50, 'mobiact': 50, 'motionsense': 100,'sisfall': 50},
                       'cnn_imu': {'mocap': 100, 'mbientlab': 100,'mobiact': 100, 'motionsense': 100,'sisfall': 100},
-                      'cnn_transformer': {'mocap': 100, 'mbientlab': 100,'mobiact': 50, 'motionsense': 100,'sisfall': 50}}
+                      'cnn_transformer': {'mocap': 100, 'mbientlab': 20,'mobiact': 50, 'motionsense': 100,'sisfall': 50}}
 
     # Number of iterations for accumulating the gradients
     accumulation_steps = {'mocap': 4, 'mbientlab': 4, 'mobiact': 4, 'motionsense': 4, 'sisfall': 4}
