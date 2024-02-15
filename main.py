fcf965380d0ed2a5d71a756f0e102cc0dc7ffe77
Nilah Ravi Nair <nilahnair.27@gmail.com>
2024-02-15 07:25:50 +0100

    network

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def load_credentials(path='~/.mongodb_credentials'):
 
 user, pw = load_credentials(path='~/.mongodb_credentials')
 
-ex= Experiment('motionsense lstm act network')
+ex= Experiment('motionsense cnntrans act network')
 
 
 
@@ -226,7 +226,7 @@ def configuration(dataset_idx, network_idx, output_idx, usage_modus_idx=0, datas
         folder_exp = {'mocap': "/data/nnair/icpr2024/lara/results/trial2/",
                     'mbientlab': "/data/nnair/icpr2024/lara_imu/results/trial2/",
                     'mobiact': "/data/nnair/icpr2024/mobiact/results/trial2/",
-                    'motionsense': "/data/nnair/icpr2024/motionsense/results/trial1/",
+                    'motionsense': "/data/nnair/icpr2024/motionsense/results/trial2/",
                     'sisfall': "/data/nnair/icpr2024/sisfall/results/trial/"
                     }
     elif output[output_idx] == 'attribute':
@@ -245,7 +245,7 @@ def configuration(dataset_idx, network_idx, output_idx, usage_modus_idx=0, datas
                     }
 
     # GPU
-    os.environ["CUDA_VISIBLE_DEVICES"] = "5"
+    os.environ["CUDA_VISIBLE_DEVICES"] = "6"
     GPU = 0
 
     # Labels position on the segmented window
@@ -358,7 +358,7 @@ def setup_experiment_logger(logging_level=logging.DEBUG, filename=None):
 def my_config():
     print("configuration function began")
     config = configuration(dataset_idx=3,
-                           network_idx=1,
+                           network_idx=3,
                            output_idx=0,
                            usage_modus_idx=0,
                            #dataset_fine_tuning_idx=0,
