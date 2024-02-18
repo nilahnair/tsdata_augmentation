d37b3dfd53b8fafbfdf04a819c7cbd2d1b170fc0
Nilah Ravi Nair <nilahnair.27@gmail.com>
2024-02-18 12:25:54 +0100

    jitter test

--- main.py
+++ main.py
@@ -245,7 +245,7 @@ def configuration(dataset_idx, network_idx, output_idx, usage_modus_idx=0, datas
                     }
 
     # GPU
-    os.environ["CUDA_VISIBLE_DEVICES"] = "4"
+    os.environ["CUDA_VISIBLE_DEVICES"] = "7"
     GPU = 0
 
     # Labels position on the segmented window
@@ -357,9 +357,9 @@ def setup_experiment_logger(logging_level=logging.DEBUG, filename=None):
 @ex.config
 def my_config():
     print("configuration function began")
-    config = configuration(dataset_idx=4,
-                           network_idx=3,
-                           output_idx=0,
+    config = configuration(dataset_idx=2,
+                           network_idx=0,
+                           output_idx=1,
                            usage_modus_idx=0,
                            #dataset_fine_tuning_idx=0,
                            reshape_input=False,
