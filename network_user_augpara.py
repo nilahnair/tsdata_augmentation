35e1a4971c33d08f064b717dc0557a23b8d80c4a
Nilah Ravi Nair <nilahnair.27@gmail.com>
2024-02-19 12:49:49 +0100

    Update network_user_augpara.py

--- network_user_augpara.py
+++ network_user_augpara.py
@@ -958,7 +958,7 @@ class Network_User(object):
                                     print('original steps')
                                     print(orig_steps)
                                     splits = np.array_split(orig_steps, num_segs)
-                                    print(splits.shape)
+                                    print(splits.size)
                                 warp = np.concatenate(np.random.permutation(splits)).ravel()
                                 augmentedData[i] = pat[warp]
                             else:
