75ee06732cce4ac2914531a79f1b682b23bd51f5
Nilah Ravi Nair <nilahnair.27@gmail.com>
2024-02-20 13:12:53 +0100

    mobiact

--- network_user_augpara.py
+++ network_user_augpara.py
@@ -870,7 +870,7 @@ class Network_User(object):
             network_obj = Network(self.config)
 
             #Loading the model
-            network_obj.load_state_dict(torch.load('/data/nnair/icpr2024/networks/cnntrans_motionsense_act.pt')['state_dict'])
+            network_obj.load_state_dict(torch.load('/data/nnair/icpr2024/networks/cnntrans_mobiact_act.pt')['state_dict'])
             network_obj.eval()
 
             logging.info('        Network_User:    Test:    setting device')
@@ -904,7 +904,7 @@ class Network_User(object):
         save_list=[]
         p=np.arange(0.01, 2, 0.02)
         #p=range(2, 20, 2)
-        with open('/data/nnair/icpr2024/augment_test/timewarp_cnntrans_motionsense.csv', 'a') as myfile:
+        with open('/data/nnair/icpr2024/augment_test/timewarp_cnntrans_mobiact.csv', 'a') as myfile:
             for aug in p:
                 print('augmentation value')
                 print(aug)
