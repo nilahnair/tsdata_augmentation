3074e7ab6eae73f66f6ac385d65b10a9516bff8b
Nilah Ravi Nair <nilahnair.27@gmail.com>
2024-02-23 13:28:16 +0100

    Update network_user_augpara.py

--- network_user_augpara.py
+++ network_user_augpara.py
@@ -932,7 +932,7 @@ class Network_User(object):
                         print(test_batch_v.dtype)
                         print(test_batch_v[0,:,0,3])
                         print(test_batch_v[-1,:,-1,3])
-                        #rand_val=test_batch_v[:, :,:, :]#np.flip(test_batch_v,axis=2)
+                        rand_val=torch.flip(test_batch_v,axis=2)
                         
                         print(test_batch_v.shape)
                         print(test_batch_v[0,:,0,3])
