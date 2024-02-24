f7fb21d72537484831bbb428f3b18ea07c03e142
Nilah Ravi Nair <nilahnair.27@gmail.com>
2024-02-24 07:22:20 +0100

    mw lara

--- main.py
+++ main.py
@@ -357,7 +357,7 @@ def setup_experiment_logger(logging_level=logging.DEBUG, filename=None):
 @ex.config
 def my_config():
     print("configuration function began")
-    config = configuration(dataset_idx=2,
+    config = configuration(dataset_idx=1,
                            network_idx=3,
                            output_idx=0,
                            usage_modus_idx=1,
