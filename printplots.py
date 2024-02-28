c8c74a2a346038aed46391594164e5472933def9
Nilah Ravi Nair <nilahnair.27@gmail.com>
2024-02-28 07:28:49 +0100

    Update printplots.py

--- printplots.py
+++ printplots.py
@@ -58,15 +58,66 @@ def aug_plot(file):
     return
 
 def groupby(groupname):
+    networks =['cnn', 'lstm', 'cnntrans']
+    dataset = ['laraimu', 'mobiact', 'motionsense', 'sisfall']
+    augmentation = ['flipping', 'jitter', 'magnitudewrap', 'permutation', 'scale', 'tilt', 'timewarp', 'windowslice']
+    
     if groupname == 'networks':
         
-    elif groupname ==
+        for i in augmentation:
+            
+            for j in networks:
+                x=[]
+                acc=[]
+                f1=[]
+                for k in dataset:
+                    file_name = i + '_' + j + '_' + k + '.csv'
+                    file = folder_base + file_name
+                    toplot=[]
+                    with open(file_name, 'r') as strlist:
+                        csvreader= csv.reader(strlist)
+                        header=next(csvreader)
+                        for i in range(len(header)):
+                            toplot.append(ast.literal_eval(header[i]))
+                        final= np.array(toplot)
+                    x_temp=[]
+                    acc_temp=[]  
+                    for i in range(final.shape[0]):
+                        x_temp.append(final[i][0])
+                        acc_temp.append(final[i][1])
+
+                    
+        
+    elif groupname == 'dataset':
+        for i in augmentation:
+            
+            for k in dataset:
+                x=[]
+                acc=[]
+                f1=[]
+                for j in networks:
+                    file_name = i + '_' + j + '_' + k + '.csv'
+                    file = folder_base + file_name
+                    toplot=[]
+                    with open(file_name, 'r') as strlist:
+                        csvreader= csv.reader(strlist)
+                        header=next(csvreader)
+                        for i in range(len(header)):
+                            toplot.append(ast.literal_eval(header[i]))
+                        
+                    
+        
+    return
 
 def main():
+    
     for root,dirs,files in os.walk(folder_base):
         for file in files:
             if file.endswith(".csv"):
                 aug_plot(file)
+    
+    
+    #groupby('networks')
         
     return
     
