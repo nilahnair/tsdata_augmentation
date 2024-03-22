4a27341074b1f717bb16579c5fb7c7736024dfb2
Nilah Ravi Nair <nilahnair.27@gmail.com>
2024-03-22 13:34:21 +0100

    Update network_user.py

--- network_user.py
+++ network_user.py
@@ -823,7 +823,7 @@ class Network_User(object):
 
                 train_batch_l = harwindow_batched["label"]
                 
-                if self.config["dtw_application"] == True and HARDataset.__random_apply__(self.augmentation_probabiblity):
+                if self.config["dtw_application"] == True and HARDataset.__random_apply__(self.config["augmentation_probabiblity"]):
                     if self.config["dtw_aug"] == 'spawner':
                         train_batch_v= aug.spawner(train_batch_v, train_batch_l, sigma=0.05, verbose=0)
                     elif self.config["dtw_aug"] == 'wdba':
