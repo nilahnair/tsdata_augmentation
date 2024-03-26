f3e371c44c3b48d495db504bcfd8eea892bc8be7
Arthur Matei <arthur.matei@gmail.com>
2024-03-26 16:08:26 +0100

    updates on main_ssl.py

--- main_ssl.py
+++ main_ssl.py
@@ -56,6 +56,7 @@ parser.add_argument('-b', '--batch-size', default=512, type=int,
                     help='mini-batch size (default: 512), this is the total '
                          'batch size of all GPUs on the current node when '
                          'using Data Parallel or Distributed Data Parallel')
+parser.add_argument('--optimizer', default='sgd', choices=['sgd','adam'])
 parser.add_argument('--lr', '--learning-rate', default=0.05, type=float,
                     metavar='LR', help='initial (base) learning rate', dest='lr')
 parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
@@ -65,7 +66,7 @@ parser.add_argument('--wd', '--weight-decay', default=1e-4, type=float,
                     dest='weight_decay')
 parser.add_argument('-p', '--print-freq', default=10, type=int,
                     metavar='N', help='print frequency (default: 10)')
-parser.add_argument('--num_checkpoints', default=8, type=int,
+parser.add_argument('--num_checkpoints', default=0, type=int,
                     help='Number of checkpoints to save during training. Saves every epochs // num_checkpoints.')
 
 parser.add_argument('--resume', default='', type=str, metavar='PATH',
@@ -167,9 +168,13 @@ def main():
     else:
         optim_params = model.parameters()
 
-    optimizer = torch.optim.SGD(optim_params, init_lr,
-                                momentum=args.momentum,
-                                weight_decay=args.weight_decay)
+    match args.optimizer:
+        case 'sgd':
+            optimizer = torch.optim.SGD(optim_params, init_lr,
+                                        momentum=args.momentum,
+                                        weight_decay=args.weight_decay)
+        case 'adam':
+            optimizer = torch.optim.Adam(optim_params, init_lr, weight_decay=args.weight_decay)
 
     # optionally resume from a checkpoint
     if args.resume:
@@ -191,7 +196,7 @@ def main():
             print("=> no checkpoint found at '{}'".format(args.resume))
     else:
         exp_identifier = '_'.join([args.dataset, args.arch, args.augmentations])
-        args.output_dir = Path(args.output_dir) / exp_identifier
+        args.output_dir = Path(args.output_dir) / exp_identifier / os.uname()[1] # add hostname to prevent merges from different hosts when copying
         run_id = 0
         while (args.output_dir / f'{run_id:03d}').exists():
             run_id +=1
@@ -229,16 +234,16 @@ def main():
         train_dataset,
         batch_size=args.batch_size,
         num_workers=args.num_workers,
-        persistent_workers=True,
+        persistent_workers=True if args.num_workers > 0 else False,
         shuffle=True,
         drop_last=True)
 
     writer = SummaryWriter(log_dir=args.output_dir)
 
     if args.resume:
-        global_step = args.start_epoch * len(train_loader)
+        log_step = args.start_epoch * len(train_loader)
     else:
-        global_step = 0
+        log_step = 0
 
     writer.add_hparams(hparam_dict=vars(args), metric_dict={})
     with open(os.path.join(args.output_dir, 'config.yaml'), 'w') as cfile:
@@ -273,7 +278,7 @@ def main():
             p1, p2, z1, z2 = model(x1=images[0], x2=images[1])
             loss = -(criterion(p1, z2).mean() + criterion(p2, z1).mean()) * 0.5
 
-            writer.add_scalar('loss', loss.item(), global_step=global_step)
+            writer.add_scalar('loss', loss.item(), global_step=log_step)
 
             losses.update(loss.item(), images[0].size(0))
 
@@ -289,7 +294,7 @@ def main():
             if i % args.print_freq == 0:
                 progress.display(i)
             
-            global_step += 1
+            log_step += 1
 
         save_path = f'checkpoint.pth.tar' 
         if args.output_dir != None:
@@ -305,21 +310,21 @@ def main():
         }, is_best=False,
             filename=save_path)
         
-        if epoch % (args.epochs // args.num_checkpoints) == 0:
-            save_path = f'checkpoint_{epoch:04d}.pth.tar' 
-            if args.output_dir != None:
-                save_path = os.path.join(args.output_dir, save_path)
-            save_checkpoint({
-                'epoch': epoch + 1,
-                'arch': args.arch,
-                'dataset': args.dataset,
-                'augmentations': args.augmentations,
-                'state_dict': model.state_dict(),
-                'encoder_state_dict': model.encoder.state_dict(),
-                'optimizer' : optimizer.state_dict(),
-                'config': vars(args)
-            }, is_best=False,
-                filename=save_path)
+        if args.num_checkpoints > 0 and epoch % (args.epochs // args.num_checkpoints) == 0:
+            # save_path = f'checkpoint_{epoch:04d}.pth.tar' 
+            # if args.output_dir != None:
+            #     save_path = os.path.join(args.output_dir, save_path)
+            # save_checkpoint({
+            #     'epoch': epoch + 1,
+            #     'arch': args.arch,
+            #     'dataset': args.dataset,
+            #     'augmentations': args.augmentations,
+            #     'state_dict': model.state_dict(),
+            #     'encoder_state_dict': model.encoder.state_dict(),
+            #     'optimizer' : optimizer.state_dict(),
+            #     'config': vars(args)
+            # }, is_best=False,
+            #     filename=save_path)
 
             save_path = f'encoder_{epoch:04d}.pth.tar' 
             if args.output_dir != None:
