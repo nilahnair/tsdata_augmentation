d9226e906e47975512c8a9021954e5ec2a183f57
Arthur Matei <arthur.matei@gmail.com>
2024-08-08 12:22:56 +0200

    lara_3s executable with main_ ssl

--- main_ssl.py
+++ main_ssl.py
@@ -131,7 +131,7 @@ def main():
         'dataset': args.dataset,
         'network': args.arch,
         'reshape_input': False, #fixed
-        'sliding_window_length': 100 if args.dataset in ['mbientlab', 'lara_mm'] and args.arch != 'cnn_transformer' else 200,
+        'sliding_window_length': 100 if args.dataset in ['mbientlab', 'lara_mm', 'lara_3s'] and args.arch != 'cnn_transformer' else 200,
         'fully_convolutional': 'FC', #fixed
         'hidden_layer': 256,
         'layer_dim': 4,
@@ -148,7 +148,7 @@ def main():
     }
     # backbone_config['NB_sensor_channels'] = {'mbientlab': 30, 'mocap': 126}.get(args.dataset, 9)
     encoder_config['NB_sensor_channels'] =   30 if args.dataset == 'mbientlab' else \
-                                             27 if args.dataset == 'lara_mm' else \
+                                             27 if args.dataset in ['lara_mm', 'lara_3s'] else \
                                             126 if args.dataset == 'mocap'     else \
                                               9
     
@@ -217,13 +217,14 @@ def main():
     dataset_root_defaults = {
         'mocap':        "/vol/actrec/DFG_Project/2019/LARa_dataset/MoCap/LARa_dataset_mocap/",
         'mbientlab':    "/vol/actrec/DFG_Project/2019/LARa_dataset/Mbientlab/LARa_dataset_mbientlab/",
-        'lara_mm':           "/vol/actrec/DFG_Project/2019/Motionminers_Dataset/DFG-Data/",
+        'lara_mm':      "/vol/actrec/DFG_Project/2018/Motionminers_Dataset/DFG-Data/",
+        'lara_3s':      "/vol/actrec/DFG_Project/2019/LARa_dataset/Motionminers/LARa_dataset_motionminers/",
         'mobiact':      "/vol/actrec/MobiAct_Dataset/",
         'motionsense':  "/vol/actrec/motion-sense-master/data/A_DeviceMotion_data/A_DeviceMotion_data",
         'sisfall':      "/vol/actrec/SisFall_dataset"
         }
     # sliding_window_length_defaults = {}
-    sliding_window_step_defaults = {'mocap': 25, 'mbientlab': 12, 'lara_mm': 12, 'mobiact': 50, 'motionsense': 25, 'sisfall': 50}
+    sliding_window_step_defaults = {'mocap': 25, 'mbientlab': 12, 'lara_mm': 12, 'lara_3s': 12, 'mobiact': 50, 'motionsense': 25, 'sisfall': 50}
 
     train_dataset = HARDataset(
         path=dataset_root_defaults[args.dataset],
